34c755a4e15763b5ef6a6478d62739d634586bf1
Richard Anderson <46491031+richardjanderson9@users.noreply.github.com>
2025-04-18 19:19:45 +0100

    Enhance App styling with new animations and remove redundant text

--- src/App.js
+++ src/App.js
@@ -13,9 +13,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
           Name: {appName}
           <br></br>
           Version: {appVersion}
