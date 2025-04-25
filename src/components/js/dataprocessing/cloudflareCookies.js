2e5a1ab85447a70d0304fc14eaeacbda92d417ad
Richard Anderson <46491031+richardjanderson9@users.noreply.github.com>
2025-04-25 19:54:39 +0100

    chore: update version to 1.2.3, add .npmcheckrc to .gitignore, and refactor CloudflareCookies module

--- src/components/js/dataprocessing/cloudflareCookies.js
+++ src/components/js/dataprocessing/cloudflareCookies.js
@@ -1,20 +1,18 @@
 // Custom Imports!
 import { getCookie } from './cookieReader';
-import cookieJson from '../../json/cookieWriter.json';
 
-// Function to set test cookies
-const setTestCookies = () => {
-    document.cookie = "env=development;path=/";
+// CloudflareCookies module object
+const CloudflareCookies = {
+    // Function to set test cookies
+    setTestCookies: () => {
+        document.cookie = "env=development;path=/";
+    },
+
+    // Function to read and pass through cookie values
+    getEnvironmentCookie: () => {
+        const envCookie = getCookie('env');
+        return envCookie;
+    }
 };
 
-// Function to read and pass through cookie values
-const getEnvironmentCookie = () => {
-    const envCookie = getCookie('env');
-    return envCookie;
-};
-
-// Export both functions
-export default {
-    setTestCookies,
-    getEnvironmentCookie
-};
+export default CloudflareCookies;
